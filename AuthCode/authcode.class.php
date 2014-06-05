accbff0ff1f73613266d541a82116898e82483db
hhqcontinue <hhqcontinue@gmail.com>
2014-06-05 11:55:43 +0800

    add comment and perfect the fontsize

--- AuthCode/authcode.class.php
+++ AuthCode/authcode.class.php
@@ -1,9 +1,9 @@
 <?php
 	/**
 	*生成验证码的类，验证码个数为4个
-	*@file authcode.class.php
-	*@author hhq
-	*@class name:AuthCode
+	* file name authcode.class.php
+	* class name:AuthCode
+	* @author hhq
 	*/
 
 	class AuthCode {
@@ -12,54 +12,82 @@
 		private $authImage;			//验证码图片
 		private $codeCount;			//验证码数量
 		private $authString;		//验证码字符串
-		
+		private $fontSize;			//字体大小
+
 		/**
-		*构造方法，初始化各个参数
-		*参数列表
-		*@param int $width	//设置验证码图片的宽度，默认值为90像素
-		*@param int $height 	//设置验证码图片的高度，默认值为30像素
-		*@param	int codeCount	//设置验证码的个数，默认值为4个
+		 * __construct function
+		 * 功能 		构造方法，初始化各个参数
+		 * @param	int		$width		//设置验证码图片的宽度，默认值为90像素
+		 * @param	int		$height		//设置验证码图片的高度，默认值为50像素
+		 * @param	int		$codeCount	//设置验证码的个数，默认值为4个
+		 * @author	hhq
 		 */
 		public function __construct($width=90, $height=30, $codeCount=4) {
 			$this->width = $width;
 			$this->height = $height;
 			$this->codeCount = $codeCount;
+			$this->fontSize = 16;
 			$this->authString = $this->createAuthString();//获得本次的验证码
 		}
-	
-		/*随机生成用户指定个数的字符串*/
-		private function createAuthString() {
-			$code = "23456789abcdefghijkmnpqrstuvwxyABCDEFGHIJKMNPQRSTUVWXY";
-			$str = "";
 
-			//生成随机验证码
+		/**
+		 * createAuthString function
+		 * 功能		随机生成用户指定个数的字符串
+		 * @access	private
+		 * @return	string
+		 * @author	hhq
+		 */
+		private function createAuthString() {
+			//验证码中可用的字符
+			$code = "23456789abcdefghijkmnpqrstuvwxyABCDEFGHIJKMNPQRSTUVWXY";
+			
+			//生成的验证码
+			$authcode = "";
+
+			//生成随机验证码,从可用字符中随机选择一个,循环codeCount次
 			for($codeIndex = 0; $codeIndex != $this->codeCount; ++$codeIndex) {
 				$authChar = $code{rand(0, strlen($code)-1)};
 
-				$str .= $authChar;
+				$authcode .= $authChar;
 			}
 
-			return $str;
+			return $authcode;
 		}
 
-		/*重写toString方法，输出验证吗图片；同时在SESSION中保存了验证码的值，使用echo方法输出对象即可看到验证码图片*/
+		/**
+		 * function __toString
+		 * 功能	重写toString方法，输出验证吗图片；同时在SESSION中保存了验证码的值，使用echo方法输出对象即可看到验证码图片
+		 * @access	public
+		 * @return	string
+		 */
 		public function __toString() {
 			$_SESSION["authcode"] = $this->authString;
-
+	
+			//输出图像
 			$this->outputImage();
 
 			return '';
 		}
 
-		/*输出图像*/
+		/**
+		 * outputImage function
+		 * 功能	输出图像
+		 * @access	public
+		 * @author	hhq
+		 */
 		private function outputImage() {
 			$this->createImage();
 			$this->setDisturbElements();
 			$this->outputText();
 			$this->renderImage();
 		}
-
-		/*创建图片资源，并初始化背景*/
+		
+		/**
+		 * createImage function
+		 * 功能	创建图片资源，并初始化背景
+		 * @access	private
+		 * @author	hhq
+		 */
 		private function createImage() {
 			$this->authImage = imagecreatetruecolor($this->width, $this->height);
 
@@ -71,10 +99,15 @@
 			imagerectangle($this->authImage, 0, 0, $this->width-1, $this->height-1, $borderColor);
 		}
 		
-		/*设置干扰因素，在图片后面添加点和线的干扰*/
+		/**
+		 * setDisturbElements function
+		 * 功能	设置干扰因素，在图片后面添加点和线的干扰
+		 * @access	private
+		 * @author	hhq
+		 */
 		private function setDisturbElements() {
-			$lineCount = 10;
-			$pointCount = 100;
+			$lineCount = 3;
+			$pointCount = 50;
 
 			for($i = 0; $i != $pointCount; ++$i) {
 				$pointColor = imagecolorallocate($this->authImage, rand(0, 255), rand(0, 255), rand(0, 255));
@@ -87,19 +120,31 @@
 						rand($this->width/2+1, $this->width-1), rand($this->height/2+1, $this->height-1), $lineColor);
 			}
 		}
-
-		/*将随机生成的字符串添加到图像中*/
+		
+		/**
+		 * outputText function
+		 * 功能	将随机生成的字符串添加到图像中
+		 * @access	private
+		 * @author	hhq
+		 */
 		private function outputText() {
 			for($i = 0; $i < $this->codeCount; ++$i) {
 				$fontColor = imagecolorallocate($this->authImage, rand(0, 128), rand(0, 128), rand(0, 128));
-				$fontSize = rand(4, 5);
 				$x = floor($this->width/$this->codeCount)*$i + 3;//为了让四个字符有足够的位置输出
-				$y = rand(0, $this->height - imagefontheight($fontSize));
-				imagechar($this->authImage, $fontSize, $x, $y, $this->authString{$i}, $fontColor);
+				$y = rand(1, $this->height - imagefontheight($this->fontSize));
+				if($y > 10) {
+					$y = 10;
+				}
+				imagechar($this->authImage, $this->fontSize, $x, $y, $this->authString{$i}, $fontColor);
 			}
 		}
-
-		/*检测GD支持的图像类型，并输出图像*/
+		
+		/**
+		 * renderImage function
+		 * 功能	检测GD支持的图像类型，并输出图像
+		 * @access	private
+		 * @author	hhq
+		 */
 		private function renderImage() {
 			if(imagetypes() & IMG_GIF) {
 				header("Content-type: image/gif");
@@ -117,10 +162,20 @@
 				trigger_error("php不支持图像创建", E_USER_ERROR);
 			}
 		}
-
-		/*析构方法，在对象结束之前自动销毁图像资源并释放内存*/
+		
+		/**
+		 * __destrut function
+		 * 功能	析构方法，在对象结束之前自动销毁图像资源并释放内存
+		 * @author	hhq
+		 */
 		public function __destruct() {
-			imagedestroy($this->authImage);
+			$y = rand(1, $this->height - imagefontheight($this->fontSize));
+			//echo $y;
+			//echo $this->fontSize;
+			//echo imagefontheight($this->fontSize);
+			if($this->authImage) {
+				imagedestroy($this->authImage);
+			}
 		}
 	}
 
