4919b8fb2983d386a75a1178f877f0d4d2fc9a44
hhqcontinue <hhqcontinue@gmail.com>
2014-06-07 17:56:04 +0800

    index.php:changeCode function with javascript

---
+++ AuthCode/index.php
@@ -0,0 +1,8 @@
+<html>
+	<a href="javascript:void(0);"><img src="testAuthcode.php" onclick="changeCode(this)"></a>
+	<script type="text/javascript">
+		function changeCode(obj) {
+			obj.src = obj.src;
+		}
+	</script>
+</html>
