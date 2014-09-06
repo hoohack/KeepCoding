9235865f483e4efd895fcc5b03c09d0a104fd8c5
hhqcontinue <hhqcontinue@gmail.com>
2014-09-06 22:09:26 +0800

    add chunk.js

--- Javascript/Library/trim.js
+++ Javascript/Library/trim.js
@@ -2,7 +2,7 @@ function trim(text) {
 	//删除头部的空白
 	text = text.replace(/^\s+/, "");
 
-	//清楚尾部的空白
+	//清除尾部的空白
 	for (var i = text.length - 1; i >= 0; i--) {
 		//当遇到非空白字符时清楚尾部空白后退出循环
 		if (/\S/.test(text.charAt(i))) {
