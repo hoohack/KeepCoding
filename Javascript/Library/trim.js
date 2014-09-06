b04f262271b210a98a330902b8b3585e65cff6b8
hhqcontinue <hhqcontinue@gmail.com>
2014-09-06 21:25:16 +0800

    trim.js

---
+++ Javascript/Library/trim.js
@@ -0,0 +1,15 @@
+function trim(text) {
+	//删除头部的空白
+	text = text.replace(/^\s+/, "");
+
+	//清楚尾部的空白
+	for (var i = text.length - 1; i >= 0; i--) {
+		//当遇到非空白字符时清楚尾部空白后退出循环
+		if (/\S/.test(text.charAt(i))) {
+			text = text.substring(0, i + 1);
+			break;
+		}
+	}
+
+	return text;
+}
