b63bcd771e283182ad428530ca534c6bd9804b4a
hhqcontinue <hhqcontinue@gmail.com>
2014-11-11 16:00:05 +0800

    php static page

--- PHP/Library/StaticPage/index.php
+++ PHP/Library/StaticPage/index.php
@@ -1,4 +1,19 @@
 <?php
-   ob_start();
+    ob_start();
 
-   ob_flush();
+    echo "<html>".
+        "<head>".
+        "<title>PHP页面静态化</title>".
+        "</head>".
+        "<body>PHP页面静态化示例</body>" .
+        "</html>";
+    $out = ob_get_contents();
+    ob_end_clean();
+    $fp = fopen("static.html", "w");
+    if (!$fp) {
+        trigger_error("System Error", E_USER_ERROR);
+    } else {
+        fwrite($fp, $out);
+        fclose($fp);
+        echo "write success";
+    }
