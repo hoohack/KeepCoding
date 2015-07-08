5f02aa82364c245813988c27897401d88466324f
hhqcontinue <hhqcontinue@gmail.com>
2015-07-08 11:32:48 +0800

    implement queue using stack

---
+++ leetcode/src/ImplementQueueUsingStacks/queue.cpp
@@ -0,0 +1,43 @@
+// Implement the following operations of a queue using stacks.
+
+// push(x) -- Push element x to the back of queue.
+// pop() -- Removes the element from in front of queue.
+// peek() -- Get the front element.
+// empty() -- Return whether the queue is empty.
+
+class Queue {
+
+public:
+    // Push element x to the back of queue.
+    void push(int x) {
+    	stack<int> tmp;
+    	while(!s.empty())
+    	{
+    		tmp.push(s.top());
+    		s.pop();
+		}
+		s.push(x);
+		while (!tmp.empty())
+		{
+            s.push(tmp.top());
+            tmp.pop();
+        }
+    }
+
+    // Removes the element from in front of queue.
+    void pop(void) {
+    	s.pop();
+    }
+
+    // Get the front element.
+    int peek(void) {
+    	return s.top();
+    }
+
+    // Return whether the queue is empty.
+    bool empty(void) {
+    	return s.empty();
+    }
+private:
+	stack<int> s;
+};
