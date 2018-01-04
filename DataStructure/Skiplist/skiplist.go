70e9af02cd2836b464fa4fb6e80cc257236819f8
hoohack <hhqcontinue@gmail.com>
2018-01-04 20:03:54 +0800

    fix

--- DataStructure/Skiplist/skiplist.go
+++ DataStructure/Skiplist/skiplist.go
@@ -1,39 +1,42 @@
-package skiplist
+package main
 
-import "math/rand"
+import (
+	"fmt"
+	"math/rand"
+)
 
-const MAX_LEVEL 32
+const MAX_LEVEL = 32
 
 type SkipNode struct {
-	key string
-	val int
-	forward[1] *SkipNode
+	key     int
+	value   string
+	forward []*SkipNode
 }
 
 type SkipList struct {
 	length int
 	header *SkipNode
-	tail *SkipNode
-	level int
+	tail   *SkipNode
+	level  int
 }
 
-func (*SkipNode) NewSkipNode() *SkipNode {
-	return &SkipNode{"", 0, nil}
+func NewSkipNode() *SkipNode {
+	return &SkipNode{0, "", nil}
 }
 
-func (this *SkipNode) MakeSkipNode(key string, val int) *SkipNode {
-	newNode := this.NewSkipNode()
+func (skiplist *SkipList) MakeSkipNode(key int, value string) *SkipNode {
+	newNode := NewSkipNode()
 	newNode.key = key
-	newNode.val = val
+	newNode.value = value
 
 	return newNode
 }
 
-func (skiplist *SkipList) NewSkipList() *SkipList {
-	newskl = &SkipList{}
+func NewSkipList() *SkipList {
+	newskl := &SkipList{}
 
 	newskl.length = 0
-	newskl.header = skiplist.NewSkipNode()
+	newskl.header = NewSkipNode()
 	newskl.tail = nil
 	newskl.level = 1
 	i := 0
@@ -45,26 +48,26 @@ func (skiplist *SkipList) NewSkipList() *SkipList {
 	return newskl
 }
 
-func (this *SkipList) CreateList() *SkipList {
+func CreateList() *SkipList {
 	return NewSkipList()
 }
 
-func (skiplist *SkipList) Insert(key string, val int) {
-	update := make([]SkipNode, MAX_LEVEL)
+func (skiplist *SkipList) Insert(key int, value string) {
+	update := make([]*SkipNode, MAX_LEVEL)
 	tmp := skiplist.header
-	i = skiplist.level
+	i := skiplist.level
 	for i >= 1 {
 		for tmp.forward[i] != nil && tmp.forward[i].key < key {
 			tmp = tmp.forward[i]
 		}
 		update[i] = tmp
-		--i
+		i--
 	}
 	tmp = tmp.forward[1]
 	if tmp.key == key {
-		tmp.val = val
+		tmp.value = value
 	} else {
-		newLvl = skiplist.RandomLevel()
+		newLvl := skiplist.RandomLevel()
 		if newLvl > skiplist.level {
 			i := skiplist.level + 1
 			for i < newLvl {
@@ -73,7 +76,7 @@ func (skiplist *SkipList) Insert(key string, val int) {
 			}
 			skiplist.level = newLvl
 		}
-		newNode = skiplist.MakeSkipNode(key, val)
+		//newNode := skiplist.MakeSkipNode(key, value)
 		i := 1
 		for i < newLvl {
 			tmp.forward[i] = update[i].forward[i]
@@ -86,9 +89,9 @@ func (this *SkipList) GetLength() int {
 	return this.length
 }
 
-func (skiplist *SkipList) Search(key string) (int, int) {
+func (skiplist *SkipList) Search(key int) (string, int) {
 	if skiplist.GetLength() == 0 {
-		return 0, 0
+		return "", 0
 	}
 	x := skiplist.header
 	for i := skiplist.level; i > 1; i-- {
@@ -99,31 +102,35 @@ func (skiplist *SkipList) Search(key string) (int, int) {
 
 	x = x.forward[1]
 	if x.key == key {
-		return x.val, 1
+		return x.value, 1
 	} else {
-		return _, 0
+		return "", 0
 	}
 }
 
-func (* SkipList) RandomLevel() int {
+func (*SkipList) RandomLevel() int {
 	level := 1
-	for rand.Intn(65535) < 0.25 * 65535 {
-		++level
+	for rand.Float64() < 0.25 {
+		level++
 	}
 
-	return level < MAX_LEVEL ? level : MAX_LEVEL
+	if level < MAX_LEVEL {
+		return level
+	} else {
+		return MAX_LEVEL
+	}
 }
 
-func (skiplist *SkipList) Delete(key string) {
+func (skiplist *SkipList) Delete(key int) {
 	update := make([]SkipNode, MAX_LEVEL)
-	tmp = skiplist.header
+	tmp := skiplist.header
 
 	i := skiplist.level
 	for i > 1 {
 		if tmp.forward[i].key < key {
 			tmp = tmp.forward[i]
 		}
-		--i
+		i--
 	}
 	tmp = tmp.forward[1]
 	if tmp.key == key {
@@ -134,7 +141,37 @@ func (skiplist *SkipList) Delete(key string) {
 			} else {
 				update[i].forward[i] = tmp.forward[i]
 			}
-			++i
+			i++
 		}
 	}
+}
+
+func main() {
+	skiplist := CreateList()
+	key1 := 1
+	value1 := "apple"
+
+	key2 := 2
+	value2 := "banana"
+
+	key3 := 3
+	value3 := "cat"
+
+	key4 := 4
+	value4 := "dog"
+
+	key5 := 5
+	value5 := "egg"
+
+	key6 := 6
+	value6 := "fruit"
+
+	skiplist.Insert(key1, value1)
+	skiplist.Insert(key2, value2)
+	skiplist.Insert(key3, value3)
+	skiplist.Insert(key4, value4)
+	skiplist.Insert(key5, value5)
+	skiplist.Insert(key6, value6)
+
+	fmt.Println(skiplist)
 }
