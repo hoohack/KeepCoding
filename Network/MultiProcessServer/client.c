a52eb45265719372cb6d5f6e389260990abaa3e9
hoohack <hhqcontinue@gmail.com>
2018-08-02 18:52:06 +0800

    fix child process exit inter signal

--- Network/MultiProcessServer/client.c
+++ Network/MultiProcessServer/client.c
@@ -1,9 +1,9 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <string.h>
-#include <arpa/inet.h>
+#include <sys/socket.h>
 #include <unistd.h>
 
 #define SERVER_PORT 9001
@@ -29,12 +29,13 @@ int main(int argc, char **argv)
 	memset(&servaddr, 0, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(SERVER_PORT);
-	if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <=0) {
+	if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
 		perror("inet_pton error");
 		exit(-1);
 	}
 
-	if (connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0) {
+	if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) <
+	    0) {
 		perror("connect");
 		exit(-1);
 	}
--- Network/MultiProcessServer/server.c
+++ Network/MultiProcessServer/server.c
@@ -1,14 +1,43 @@
-#include <stdio.h>
-#include <sys/socket.h>
-#include <stdlib.h>
-#include <netinet/in.h>
-#include <string.h>
-#include <unistd.h>
 #include <arpa/inet.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 #define SERVER_PORT 9001
 #define MAX_BUFF_SIZE 4096
 #define MAX_BACK_LOG 1024
+
+static void sigchld_handler(int sig)
+{
+	int status;
+	while (waitpid(-1, &status, WNOHANG) > 0)
+		;
+}
+
+int handler_request(int client_fd)
+{
+	char buff[MAX_BUFF_SIZE];
+	int size = 0;
+	while ((size = read(client_fd, buff, MAX_BUFF_SIZE)) != 0) {
+		if (size == -1) {
+			if (errno == EINTR) {
+				continue;
+			}
+
+			perror("Fail to recv from client");
+			return errno;
+		}
+	}
+
+	printf("recv msg from client_%d: %s\n", client_fd, buff);
+	return 0;
+}
 
 int main()
 {
@@ -33,15 +62,16 @@ int main()
 		exit(-1);
 	}
 
-	/* 使端口马上可用 */
+	/* make port reusable */
 	int on = 1;
 	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(int)) <
-			0) {
+	    0) {
 		perror("setsockopt");
 		exit(-1);
 	}
 
-	if (bind(server_fd, (struct sockaddr*)&servaddr, sizeof(servaddr)) == -1) {
+	if (bind(server_fd, (struct sockaddr *)&servaddr, sizeof(servaddr)) ==
+	    -1) {
 		perror("bind error");
 		exit(-1);
 	}
@@ -51,32 +81,42 @@ int main()
 		exit(-1);
 	}
 
+	// register SIGCHLD handler
+	struct sigaction chld_action;
+	chld_action.sa_handler = sigchld_handler;
+	chld_action.sa_flags = SA_NODEFER;
+	if (sigaction(SIGCHLD, &chld_action, NULL) == -1) {
+		perror("Oh! Can not catch SIGCHLD signal. :) ");
+		exit(-1);
+	}
+
 	printf("server running...listen port %d\n", SERVER_PORT);
 
 	pid_t pid = 0;
 
-	while(1) {
-		if ((conn_fd = accept(server_fd, (struct sockaddr *)&clientaddr, &addr_len)) == -1) {
+	while (1) {
+		if ((conn_fd = accept(server_fd, (struct sockaddr *)&clientaddr,
+				      &addr_len)) == -1) {
+			if (errno == EINTR) {
+				continue;
+			}
 			perror("accept error");
 			continue;
 		}
 
-		pid = fork();
-		if (pid < 0) {
+		if ((pid = fork()) < 0) {
 			perror("fork error");
 			exit(-1);
 		} else if (pid == 0) {
-			printf("fork a process to deal with new connect\n");
-			printf("client %d connected, waiting for data\n", conn_fd);
-
-			n = read(conn_fd, buff, MAX_BUFF_SIZE);
-			buff[n] = '\0';
-			printf("recv msg from client_%d: %s\n", conn_fd, buff);
-			close(conn_fd);
-		} else {
-			continue;
+			if (handler_request(conn_fd) == 0) {
+				return 0;
+			} else {
+				perror("handle request");
+				exit(-1);
+			}
 		}
 	}
 
 	close(server_fd);
+	return 0;
 }
